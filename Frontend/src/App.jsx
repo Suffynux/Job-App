a5177b396b1e0d6ae8ceb19e5fac46ccab8e4995
Sufiyan <suffynux@gmail.com>
2025-03-30 04:54:46 +0500

    Compoenents added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,44 +1,59 @@
-import React, { useContext , useEffect } from 'react'
-import "./App.css"
-import { Context } from './main.jsx'
-import axios from "axios";
+import React, { useContext } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
 import Login from './Components/Auth/Login.jsx';
 import Register from './Components/Auth/Register.jsx';
-import Navbar from './Components/Layout/Navbar.jsx';
-import Footer from './Components/Layout/Footer.jsx';
 import Home from './Components/Home/Home.jsx';
 import Job from './Components/Job/Job.jsx';
 import JobDetails from './Components/Job/JobDetails.jsx';
 import MyJobs from './Components/Job/MyJobs.jsx';
 import PostJob from './Components/Job/PostJob.jsx';
 import Application from './Components/Application/Application.jsx';
-import MyApplicatoins from './Components/Application/MyApplicatoins.jsx';
-import ResumeModel from './Components/Application/ResumeModel.jsx';
+import MyApplications from './Components/Application/MyApplications.jsx';
 import NotFound from './Components/NotFound/NotFound.jsx';
-import {Toaster} from "react-hot-toast"
-import { BrowserRouter as Router , Route , Routes} from 'react-router-dom';
+import { Toaster } from "react-hot-toast";
+import { Context } from './main.jsx';
+import axios from "axios";
+
 function App() {
+  const {isAuthorized , setIsAuthorized , user , setUser} = useContext(Context)
+  useEffect(() => {
+    const fetchUser = async()=>{
+      try {
+        const response = await axios.get("" , {withCredentials: true})
+        setUser(response.data.user)
+        setIsAuthorized(true)
+      } catch (error) {
+        setIsAuthorized(false)
+        console.log("Getting error while fetching user" , error);
+      }
+    }
+    fetchUser() 
+  }, [isAuthorized])
+  
   return (
-    <>    
-    <Router>
+    <BrowserRouter>
+      <Toaster />
       <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/job/getalljobs' element={<JobDetails/>}/>
-        <Route path='/job/:id' element={<Job/>}/>
-        <Route path='/job/post' element={<PostJob/>}/>
-        <Route path='/job/me' element={<MyJobs/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        
+        {/* Job Routes */}
+        <Route path='/jobs' element={<JobDetails />} />
+        <Route path='/job/:id' element={<Job />} />
+        <Route path='/job/post' element={<PostJob />} />
+        <Route path='/job/me' element={<MyJobs />} />
 
-        <Route path='/application/:id' element={<Application/>}/>
-        <Route path='/application/me' element={<MyApplicatoins/>}/>
+        {/* Application Routes */}
+        <Route path='/application/:id' element={<Application />} />
+        <Route path='/application/me' element={<MyApplications />} />
 
-        <Route path="*" element={<NotFound/>}/>
-
+        {/* 404 Route */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </Router>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
