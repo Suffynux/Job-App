424c6cc5524a61858e72a5d2249a6687726442b3
Sufiyan <suffynux@gmail.com>
2025-04-07 19:59:06 +0500

    Send Application intregation

--- Frontend/src/Components/Application/Application.jsx
+++ Frontend/src/Components/Application/Application.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../../main.jsx";
+import { useFetcher, useParams } from "react-router-dom";
+import axios from "axios";
 function Application() {
-  return (
-    <div>Application</div>
-  )
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [coverLetter, setCoverLetter] = useState("");
+  const [address, setAddress] = useState("");
+  const [resume, setResume] = useState(null);
+
+  const { isAuthorized, user } = useContext(Context);
+  const navigateTo = useNavigate();
+
+  if (!isAuthorized) {
+    navigateTo("/");
+  }
+
+  // function for handle file input
+  const handleFileInput = (e) => {
+    const resume = req.target.files[0];
+    setResume(resume);
+
+  };
+  
+  // getting job id from the user
+  const {id} = useParams();
+
+  // function for seding request
+  const handleApplication = async (e) => {
+    e.prevantDefault();
+    const formData= new FormData();
+    
+  }
+  return <div></div>;
 }
 
-export default Application
+export default Application;
